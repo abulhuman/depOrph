e5fe27d0c476965a7b52257ae36b9b583ce39e65
Adem Mohammed <adem164416@gmail.com>
2020-08-27 15:11:44 +0300

    all types defined in schema.graphql

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
-import PrismaClient from '@prisma/client'   
+const { PrismaClient } = require('@prisma/client');
+const { GraphQLServer } = require('graphql-yoga');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
 
 const prisma = new PrismaClient();
 
-prisma
+const resolvers = {
+  Query,
+  Mutation,
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+    };
+  },
+});
+
+server.start();
